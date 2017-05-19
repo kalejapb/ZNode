5b709d4d76faa1dd1076d5710df36844c94305ad
Alejandra Prieto <alejandra.prieto@morningstar.com>
2017-05-19 09:17:51 -0500

    Examples.

---
+++ Examples/imc-calculator/app.js
@@ -0,0 +1,6 @@
+//var moment = require("moment");
+//console.log( moment().format("LLLL") );
+
+var calc = require("./utils/sum");
+console.log(calc.sum(2,3));
+console.log(calc.multiply(2,3));
---
+++ Examples/imc-calculator/utils/sum.js
@@ -0,0 +1,12 @@
+var sum = function(a, b){
+  return a+b;
+}
+
+var multiply = function(a, b){
+  return a*b;
+}
+
+exports.sum = sum;
+exports.multiply = multiply;
+
+
