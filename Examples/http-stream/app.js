ad8a61d2371dc2e939a2e1b23f44c6ceda9a2316
Alejandra Prieto <alejandra.prieto@morningstar.com>
2017-07-26 11:37:11 -0500

    Http and request examples and tests.

--- Examples/http-stream/app.js
+++ Examples/http-stream/app.js
@@ -3,8 +3,8 @@ const http = require('http');
 const server = http.createServer(function (req, res) {
   console.log("Creating server");
   req.pipe(process.stdout);
-
-  res.end();
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end("bye");
 });
 
 server.listen(8080);
