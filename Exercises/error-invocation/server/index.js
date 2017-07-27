8d71551a0510e7724dd23c819268128b4a3607e0
Alejandra Prieto <alejandra.prieto@morningstar.com>
2017-07-27 16:49:13 -0500

    Exercise 8

---
+++ Exercises/error-invocation/server/index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const express = require('express');
+const morgan = require('morgan');
+const logger = require('winston');
+
+const app = express();
+// Setup middleware
+app.use(morgan('common'));
+
+app.get('/', (req, res, next) => {
+  logger.info("Home page");
+  res.json({
+    "message": "Welcome to the API"
+  });
+});
+
+app.get('/info', (req, res, next) => {
+  logger.info("A page");
+  throw new Error('Oh oh! Error invocation working :)');
+  res.json({
+    "message": "Error invocation exercise - Aleja"
+  });
+});
+
+app.use( (req, res, next) => {
+  logger.info("Route not found");
+  res.status(404);
+  res.json({
+    "error": "Error. Route not found"
+  });
+});
+
+app.use( (err, req, res, next) => {
+  logger.error("Error");
+  res.status(500);
+  res.json({
+    "error": `${err}`
+  });
+});
+
+module.exports = app;
